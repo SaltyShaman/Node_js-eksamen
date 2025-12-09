4e9bdb09a62405f825c3829c619bea2a4f901cb0
SaltyShaman <kristoffer6400@gmail.com>
2025-12-09 14:20:09 +0100

    Generel session limits backend setup

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,27 @@ const app = express();
 
 // Config
 import sessionConfig from "./config/sessionConfig.js";
+import { generalLimiter, authLimiter } from "./config/rateLimiters.js";
+
 
 //Middleware imports
 import helmet from "helmet";
 
 app.use(express.json());
 
+import cors from 'cors';
+
+//CORS has to be before the routers
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+
 // Middleware app use
 app.use(sessionConfig);
 app.use(helmet());
+app.use(generalLimiter);
 
 
 // Server
